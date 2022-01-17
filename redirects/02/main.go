971514a42449931e57119fa30dc3649163cd3642
paul <paulchibiukeigweze@gmail.com>
2022-01-18 00:50:54 +0100

    Go redirects and Http Status

---
+++ redirects/02/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/bar", bar)
+	http.HandleFunc("/barred", barred)
+
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Your request method at foo:", r.Method, "\n\n")
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("Your request method at bar:", r.Method, "\n\n")
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+
+}
+
+func barred(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("Your request method at barrred:", r.Method, "\n\n")
+
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
