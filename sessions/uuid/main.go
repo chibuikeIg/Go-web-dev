4dc9e089162e83bc20709c91a6903d434c577c19
paul <paulchibiukeigweze@gmail.com>
2022-01-25 02:06:06 +0100

    creating sessions

---
+++ sessions/uuid/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	exPackage "github.com/satori/go.uuid"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+
+	cookie, err := r.Cookie("session")
+
+	if err != nil {
+
+		id := exPackage.NewV4()
+
+		cookie = &http.Cookie{
+			Name:  "session",
+			Value: id.String(),
+			//Secure: true,
+			HttpOnly: true,
+		}
+
+		http.SetCookie(w, cookie)
+
+	}
+
+	fmt.Println(cookie)
+
+}
