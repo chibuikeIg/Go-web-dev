e416edb089ce5a77a667768513524c835a37d249
paul <paulchibiukeigweze@gmail.com>
2022-02-06 02:32:50 +0100

    create aws instance and deployed hello_world_app

---
+++ aws/hello_world_app/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+
+	http.ListenAndServe(":80", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Oh yeah, I'm running on AWS.")
+}
