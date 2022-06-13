520f5f0b33b70bbc57a2eb1644cea78f5029aff2
chibuikeIg <paulchibiukeigweze@gmail.com>
2022-06-13 14:41:44 +0100

    go and sql in practice: fetch names from database

--- go-and-sql/main.go
+++ go-and-sql/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"io"
 	"net/http"
 
@@ -13,7 +14,7 @@ var err error
 
 func main() {
 
-	db, err = sql.Open("mysql", "username:password@tcp(endpoint3306)/schema_name?charset=utf8")
+	db, err = sql.Open("mysql", "username:password@tcp(endpoint:3306)/database?charset=utf8")
 
 	check(err)
 
@@ -24,6 +25,7 @@ func main() {
 	check(err)
 
 	http.HandleFunc("/", Index)
+	http.HandleFunc("/names", Names)
 
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 
@@ -35,6 +37,29 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	_, err = io.WriteString(w, "Successfully connected")
 
 	check(err)
+}
+
+func Names(w http.ResponseWriter, r *http.Request) {
+
+	rows, err := db.Query(`SELECT name FROM users`)
+
+	check(err)
+
+	var s, name string
+
+	defer rows.Close()
+
+	for rows.Next() {
+
+		err = rows.Scan(&name)
+		check(err)
+
+		s += name + "\n"
+
+	}
+
+	fmt.Fprintln(w, s)
+
 }
 
 func check(err error) {
