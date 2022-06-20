47378d66703dd055aa54ec35694a42a0f2c1f4f8
chibuikeIg <paulchibiukeigweze@gmail.com>
2022-06-20 13:50:24 +0100

    Aws Load balancer

--- aws/load-balancer/main.go
+++ aws/load-balancer/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
 	"io"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
+var db *sql.DB
+var err error
+
 func main() {
+
+	db, err = sql.Open("mysql", "user:password@tcp(endpoint:3306)/database?charset=utf8")
 
 	http.HandleFunc("/", Index)
 	http.HandleFunc("/ping", Ping)
 	http.HandleFunc("/instance", Instance)
-
+	http.HandleFunc("/users", GetUsers)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 
 	http.ListenAndServe(":80", nil)
@@ -28,12 +35,34 @@ func Ping(w http.ResponseWriter, r *http.Request) {
 
 func Instance(w http.ResponseWriter, r *http.Request) {
 
+	io.WriteString(w, GetInstance())
+}
+
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+
+	rows, err := db.Query(`SELECT name FROM users`)
+	check(err)
+	defer rows.Close()
+
+	s := GetInstance()
+
+	s += "\n Retrieved Records\n"
+	var name string
+
+	for rows.Next() {
+		err := rows.Scan(&name)
+		check(err)
+		s += name
+	}
+
+	io.WriteString(w, s)
+
+}
+
+func GetInstance() string {
 	resp, err := http.Get("http://169.254.169.254/latest/meta-data/instance-id")
 
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	check(err)
 
 	bs := make([]byte, resp.ContentLength)
 
@@ -41,6 +70,12 @@ func Instance(w http.ResponseWriter, r *http.Request) {
 
 	resp.Body.Close()
 
-	io.WriteString(w, string(bs))
+	return string(bs)
+}
 
+func check(err error) {
+	if err != nil {
+		panic(err)
+		return
+	}
 }
